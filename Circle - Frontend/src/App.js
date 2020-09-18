61e3a942eee6388bc7573cba4e817be4fa925dc1
Nuo (Tony) Chen <chen2886@purdue.edu>
2020-09-17 23:43:17 -0400

    Added app bar

--- Circle - Frontend/src/App.js
+++ Circle - Frontend/src/App.js
@@ -1,12 +1,132 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from "@material-ui/icons/Menu";
+import Helmet from "react-helmet";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    backgroundColor: "#528487",
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "30ch",
+    },
+    alignContent: "center",
+  },
+}));
+
+const font = "'Tenor Sans', sans-serif";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: font,
+  },
+});
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
   return (
-    <div className="App">
-
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.grow}>
+        <Helmet>
+          <title>Home</title>
+          <style>{"body { background-color: black; }"}</style>
+        </Helmet>
+        <div className={classes.root}>
+          <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+              <Grid justify="space-between" container alignItems="center" direction="row">
+                <Grid xs={2} container alignItems="center" direction="row">
+                  <IconButton
+                    edge="start"
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="menu"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" className={classes.title}>
+                    Circle
+                  </Typography>
+                </Grid>
+                <Grid xs={2} item></Grid>
+                <Grid xs={4} item>
+                  <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                      <SearchIcon />
+                    </div>
+                    <InputBase
+                      placeholder="Search…"
+                      classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                      }}
+                      inputProps={{ "aria-label": "search" }}
+                    />
+                  </div>
+                </Grid>
+                <Grid xs={3} item></Grid>
+                <Grid xs={1} item>
+                  <div align="right">
+                    <Button color="inherit">Login</Button>
+                  </div>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </div>
+    </MuiThemeProvider>
   );
 }
-
-export default App;
